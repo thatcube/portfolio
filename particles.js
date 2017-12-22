9f935b257301ceaa381939c5c3ecafcb1d23d06c
thatcube <brandon-moore@live.com>
2017-12-22 18:38:58 -0500

    added JSON to CDN

--- particles.js
+++ particles.js
@@ -1,4 +1,4 @@
-particlesJS.load('particles', 'src/particlesjs-config.json', function() {
+particlesJS.load('particles', 'https://cdn.jsdelivr.net/gh/thatcube/portfolio/src/particlesjs-config.json', function() {
     console.log('Hey there! If you\'re interested in working with me, shoot me an email: mooreb@alfredstate.edu');
     console.log('Thanks for checking out my stuff!');
 });
